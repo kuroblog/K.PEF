d052de06f123c6b7eb1448c7c8234119cf1eb7a4
Thinker.Q <kuro_q@live.com>
2021-01-02 15:19:13 +0800

    init k.pef.shell

---
+++ src/K.PEF/K.PEF.Shell/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+namespace K.PEF.Shell
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            DispatcherUnhandledException += (s, o) => { };
+
+            TaskScheduler.UnobservedTaskException += (s, o) => { };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, o) => { };
+
+            StartupUri = new Uri($"{nameof(MainWindow)}.xaml", UriKind.Relative);
+
+            base.OnStartup(e);
+        }
+    }
+}
