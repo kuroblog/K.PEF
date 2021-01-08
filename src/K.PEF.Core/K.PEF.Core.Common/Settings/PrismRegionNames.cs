8c3c3045a49b3f024898998a959f33b464e8de4a
Thinker.Q <kuro_q@live.com>
2021-01-08 16:57:19 +0800

    添加并设置用作容器窗口的 RegionName

---
+++ src/K.PEF.Core/K.PEF.Core.Common/Settings/PrismRegionNames.cs
@@ -0,0 +1,11 @@
+﻿namespace K.PEF.Core.Common.Settings
+{
+    public class PrismRegionNames
+    {
+        public static string MainRegion { get; } = nameof(MainRegion);
+
+        public static string ContentRegion { get; } = nameof(ContentRegion);
+
+        public static string HomeRegion { get; } = nameof(HomeRegion);
+    }
+}
