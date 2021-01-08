64d89d3ac03cb5ae8a8c0517037c5839769ccea8
Thinker.Q <kuro_q@live.com>
2021-01-09 01:39:47 +0800

    清理项目, 基础框架完成

--- src/K.PEF.Core/K.PEF.Core.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace K.PEF.Core.Common
-{
-    public class Class1
-    {
-    }
-}
--- src/K.PEF.Core/K.PEF.Core.Shell/ViewModels/ShellWindowViewModel.cs
+++ src/K.PEF.Core/K.PEF.Core.Shell/ViewModels/ShellWindowViewModel.cs
@@ -1,35 +1,10 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
-using Prism.Regions;
 
 namespace K.PEF.Core.Shell.ViewModels
 {
-    public partial class ShellWindowViewModel : BindableBase, INavigationAware
+    public partial class ShellWindowViewModel : BindableBase
     {
-        /****************************************************************************************************
-         * Prism.Regions.INavigationAware
-         * {
-         *      bool IsNavigationTarget(Prism.Regions.NavigationContext navigationContext);
-         * }
-         * **************************************************************************************************/
-        public bool IsNavigationTarget(NavigationContext navigationContext) => true;
-
-        /****************************************************************************************************
-         * Prism.Regions.INavigationAware
-         * {
-         *      void OnNavigatedFrom(Prism.Regions.NavigationContext navigationContext);
-         * }
-         * **************************************************************************************************/
-        public void OnNavigatedFrom(NavigationContext navigationContext) { }
-
-        /****************************************************************************************************
-         * Prism.Regions.INavigationAware
-         * {
-         *      void OnNavigatedTo(Prism.Regions.NavigationContext navigationContext);
-         * }
-         * **************************************************************************************************/
-        public void OnNavigatedTo(NavigationContext navigationContext) { }
-
         public ShellWindowViewModel() { }
 
         public DelegateCommand<object> LoadedCommand => new DelegateCommand<object>(args => { });
