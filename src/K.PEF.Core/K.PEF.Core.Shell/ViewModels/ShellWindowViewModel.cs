9ec4494134d2a970430fc256fe15b8dbd3973d7f
Thinker.Q <kuro_q@live.com>
2021-01-08 16:49:57 +0800

    添加默认窗口的 ViewModel

---
+++ src/K.PEF.Core/K.PEF.Core.Shell/ViewModels/ShellWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace K.PEF.Core.Shell.ViewModels
+{
+    public partial class ShellWindowViewModel : BindableBase
+    {
+        public ShellWindowViewModel()
+        {
+        }
+
+        public DelegateCommand<object> LoadedCommand => new DelegateCommand<object>(args =>
+        {
+        });
+    }
+}
