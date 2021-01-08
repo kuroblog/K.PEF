16fddc438da76c5abadba064330c5f98c9f86408
Thinker.Q <kuro_q@live.com>
2021-01-08 16:41:05 +0800

    1. 使用 PrismApplication 替换 App 的默认基类 2. 替换 App.xaml

--- src/K.PEF.Core/K.PEF.Core.Shell/App.xaml.cs
+++ src/K.PEF.Core/K.PEF.Core.Shell/App.xaml.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace K.PEF.Core.Shell
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    //public partial class App : Application
+    // 使用 Prism.Unity.PrismApplication 替换 System.Windows.Application
+    public partial class App : PrismApplication
     {
+        /****************************************************************************************************
+         * Prism.PrismApplicationBase
+         * {
+         *      protected abstract void RegisterTypes(Prism.Ioc.IContainerRegistry containerRegistry);
+         * }
+         * **************************************************************************************************/
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        /****************************************************************************************************
+         * Prism.PrismApplicationBase
+         * {
+         *      protected abstract System.Windows.Window CreateShell()
+         * }
+         * **************************************************************************************************/
+        protected override Window CreateShell()
+        {
+            var shell = Container.Resolve<MainWindow>();
+            shell.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            return shell;
+        }
     }
 }
